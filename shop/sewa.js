38d1bb11e2c28c7cdf3fbba6f60bc00c411f6f7a
AldoBotz <aldomt724@gmail.com>
2022-02-03 21:53:08 +0700

    tes bang v3

---
+++ shop/sewa.js
@@ -0,0 +1,23 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = Rp 5.000
+⬡ 2 MINGGU = Rp 8.000
+⬡ 3 MINGGU = Rp 10.000
+⬡ 1 BULAN = Rp 15.000
+⬡ PERMANEN = Rp 50.000
+⬡ PERMANEN + USER PREM = Rp 120.000
+
+
+
+⬣ MINAT? PM
+⬡ wa.me/6281317549796
+
+
+NOTE : 
+Bot On 24 Jam `
+	}
+
+exports.pc_sewa = pc_sewa
